008b2c2485039dfb310c2a157484274dd4367529
farhanakhanswe <farhanakhanswe@gmail.com>
2023-11-09 00:50:39 +0600

    factories and seeders set up

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,25 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Tag;
+use App\Models\User;
+use App\Models\Article;
 use Illuminate\Database\Seeder;
+use Database\Factories\TagFactory;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(): void // run: php artisan db:seed
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(CategorySeeder::class); 
+        User::factory()->count(10)->create();
+        Article::factory()
+                ->has(Tag::factory()->count(2))
+                ->count(50)
+                ->create();
     }
 }
